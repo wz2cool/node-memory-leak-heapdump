ac3bf4aaffdae0dffea5e4556cac6aa9a5c7266b
wz2cool <wz2cool@live.cn>
2017-09-27 15:20:05 +0800

    add test

--- src/heapdump.ts
+++ src/heapdump.ts
@@ -10,7 +10,7 @@ export class Heapdump {
         const dir = path.dirname(filepath);
         const deferred = q.defer<string>();
         this.createDirIfNotExists(dir)
-            .then((dirpath) => {
+            .then((createNew) => {
                 heapdump.writeSnapshot(filepath, (err) => {
                     if (util.isNullOrUndefined(err)) {
                         deferred.resolve(filepath);
@@ -24,15 +24,15 @@ export class Heapdump {
         return deferred.promise;
     }
 
-    private static createDirIfNotExists(dirpath: string): Promise<string> {
-        const deferred = q.defer<string>();
+    private static createDirIfNotExists(dirpath: string): Promise<boolean> {
+        const deferred = q.defer<boolean>();
         if (fs.existsSync(dirpath)) {
-            deferred.resolve(dirpath);
+            deferred.resolve(false);
         } else {
             mkdirp(dirpath, (err) => {
                 if (util.isNullOrUndefined(err)) {
                     // create dir success.
-                    deferred.resolve(dirpath);
+                    deferred.resolve(true);
                 } else {
                     deferred.reject(err);
                 }
---
+++ test/heapdumpTest.ts
@@ -0,0 +1,53 @@
+import { expect } from "chai";
+import * as mkdirp from "mkdirp";
+import * as os from "os";
+import * as path from "path";
+import * as q from "q";
+import * as rimraf from "rimraf";
+import * as util from "util";
+import { Heapdump } from "./../src/heapdump";
+
+describe("#createDirIfNotExists", () => {
+    const dumpDir = path.join(os.tmpdir(), "heapdumpTest");
+    const filepath = path.join(dumpDir, "test.heapsnapshot");
+    console.log("dumpDir:", dumpDir);
+    console.log("filepath", filepath);
+
+    it("should create new directory to save file", (done) => {
+        rimraf(dumpDir, (err) => {
+            if (util.isNullOrUndefined(err)) {
+                const result = (Heapdump as any).createDirIfNotExists(dumpDir) as q.Promise<boolean>;
+                result.then((createNew) => {
+                    if (createNew) {
+                        done();
+                    } else {
+                        done("should create new dir");
+                    }
+                }).catch((error) => {
+                    done(error);
+                });
+            } else {
+                done(err);
+            }
+        });
+    });
+
+    it("should not create new directory since directory exists", (done) => {
+        mkdirp(dumpDir, (err) => {
+            if (util.isNullOrUndefined(err)) {
+                const result = (Heapdump as any).createDirIfNotExists(dumpDir) as q.Promise<boolean>;
+                result.then((createNew) => {
+                    if (createNew) {
+                        done("should not create new dir");
+                    } else {
+                        done();
+                    }
+                }).catch((error) => {
+                    done(error);
+                });
+            } else {
+                done(err);
+            }
+        });
+    });
+});
